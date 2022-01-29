fdeb9506065051f1889319a0c8ba97f7f46b1fdb
Pieter van Zutven <pietervanzutven@outlook.com>
2022-01-29 20:11:16 +0100

    Add project files.

---
+++ Authenticator/js/main.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+window.onload = () => {
+
+    let totp = new OTPAuth.TOTP({
+	    issuer: 'ACME',
+	    label: 'AzureDiamond',
+	    algorithm: 'SHA1',
+	    digits: 6,
+	    period: 30,
+	    secret: 'NB2W45DFOIZA'
+    });
+
+    let token = totp.generate();
+
+
+
+}
